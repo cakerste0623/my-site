b97ce37f967ac32e775c00790265d8a7d75f563a
cakerste0623 <cak0623@gmail.com>
2023-02-05 16:58:50 -0500

    added boilerplate vue project, added necessary docker/nginx componenets

--- backend/server.py
+++ backend/server.py
@@ -47,5 +47,9 @@ def test():
     t = parseDataAndPush(track)
     return jsonify(t), HTTPStatus.OK
 
+@app.route('/check', methods=['GET'])
+def check():
+    return "Check successful"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9090, debug=True)
