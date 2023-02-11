bd9282c61edaf293c2c3cce1f4ac598c0bebef71
cakerste0623 <cak0623@gmail.com>
2023-02-10 23:20:16 -0500

    rebuilt vue project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    HelloWorld,
   },
-  mounted() {
-    axios.get('/api/check').then(response => (console.log(response)))
-  }
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
