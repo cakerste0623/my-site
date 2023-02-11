4f43b7e92b8af01f6f1a5cec12dfb8073c1b32a6
cakerste0623 <cak0623@gmail.com>
2023-02-11 17:35:13 -0500

    added routing for a song page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,41 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <router-view />
+      <v-app-bar
+      color="primary"
+      density="compact"
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar-title>Welcome</v-app-bar-title>
+      </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      location="left"
+      temporary
+    >
+      <v-list>
+        <v-list-item prepend-icon="mdi-home-city" title="Home" value="home" link @click="$router.push({ path: '/' })"></v-list-item>
+        <v-list-item prepend-icon="mdi-music" title="What am I listening to?" value="song" link @click="$router.push({ path: '/song' })"></v-list-item>
+        <v-list-item prepend-icon="mdi-file-account" title="Resume" value="resume"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import axios from "axios"
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+
   },
 
   data: () => ({
-    //
+    drawer: false
   }),
 
   mounted() {
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,23 +1,5 @@
 <template>
   <v-container>
-    <v-app-bar
-      color="primary"
-      density="compact"
-    >
-      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-app-bar-title>Welcome</v-app-bar-title>
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      location="left"
-      temporary
-    >
-      <v-list>
-        <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
-        <v-list-item prepend-icon="mdi-music" title="What am I listening to?" value="song" link @click="$router.push({ path: '/song' })"></v-list-item>
-        <v-list-item prepend-icon="mdi-file-account" title="Resume" value="resume"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
