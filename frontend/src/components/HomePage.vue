7856b8a2fefc409f3a3deb47e703eb1344a10d88
cakerste0623 <cak0623@gmail.com>
2023-02-28 22:35:21 -0500

    changed home page

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -3,26 +3,21 @@
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
-          :src="require('../assets/logo.svg')"
+          :src="require('../assets/myself.jpg')"
           class="my-3"
           contain
-          height="200"
+          height="300"
         />
       </v-col>
 
-      <v-col class="mb-4">
+      <v-col class="mx-16">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Beta
+          Collin Kersten
         </h1>
 
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          I have a bachelor's degree in computer science with a concentration in cybersecurity. I have one year of experience in the software engineering field. I have experience working with Kubernetes, Helm, Azure, Docker, Jenkins, Ansible, Java, Python, and Bash. For my full resume, visit the resume page on this site.
         </p>
       </v-col>
 
@@ -30,63 +25,9 @@
         class="mb-5"
         cols="12"
       >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+        <v-btn prepend-icon="mdi-email" append-icon="mdi-email" href="mailto:cak0623@gmail.com" class="my-12" size="x-large">
+          Contact Me!
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
