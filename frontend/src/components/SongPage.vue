ab98054140959434ea28d31a7222b53404b767b8
cakerste0623 <cak0623@gmail.com>
2023-03-11 14:56:40 -0500

    resolved spacing issues on song page

--- frontend/src/components/SongPage.vue
+++ frontend/src/components/SongPage.vue
@@ -1,13 +1,10 @@
 <template>
-  <v-app>
     <v-container>
       <v-row class="x-row grey lighten-5" align="start" justify="center">
         <h1 class="display-2 font-weight-bold mb-3">
           What am I listening to?
         </h1>
       </v-row>
-    </v-container>
-    <v-container>
       <v-row class="x-row grey lighten-5" align="start">
         <v-col cols=6 align="right">
             <v-img width="300" :src=song.coverImage>
@@ -35,27 +32,26 @@
         </v-col>
       </v-row>
       <v-row class="mt-8" justify="center">
-        <h1 class="display-2 font-weight-bold mb-3">
+        <h1 class="display-2 font-weight-bold">
           What is this?
         </h1>
       </v-row>
-      <v-row>
+      <v-row class="my-4">
         <h3>
           Well, if you know anything about me, I'm pretty much listening to music all day everyday. I'm using this page to show what my favorite song is at the moment. Hope you can find a new favorite song here :)
         </h3>
       </v-row>
-      <v-row class="mt-8" justify="center">
+      <v-row class="mb-4" justify="center">
         <h1>
           How does it work?
         </h1>
       </v-row>
-      <v-row>
+      <v-row class="mb-4">
         <h3>
           This site has a few APIs running in the backend. The POST version of the API adds a song to the database, and then the GET version of the API retrieves the most recently added song from the database. The POST API requires a JWT that can only be acquired through a different login API, so only I can hit that API.
         </h3>
       </v-row>
     </v-container>
-  </v-app>
 </template>
 
 <script>
