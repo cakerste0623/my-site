4bdb1be716d6103f08d2348cc3a2d86ce814c367
cakerste0623 <cak0623@gmail.com>
2023-03-11 17:09:18 -0500

    finished resume page

--- frontend/src/components/ResumePage.vue
+++ frontend/src/components/ResumePage.vue
@@ -1,5 +1,12 @@
 <template>
     <v-container>
-        <h1>Resume Page</h1>
+        <v-row class="text-center" align="start">
+            <v-col cols="12">
+                <a href="./assets/resume.pdf" download="CollinKerstenResume">
+                    <h1>Download Here!</h1>
+                </a>
+                <embed src="./assets/resume.pdf" type="application/pdf" height="700" width="500">
+            </v-col>
+        </v-row>
     </v-container>
 </template>
