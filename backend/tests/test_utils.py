60ed036a84dabf2d7fbb74f4f1b0a496989a3feb
cakerste0623 <cak0623@gmail.com>
2023-04-09 21:36:19 -0400

    started writing unit tests

---
+++ backend/tests/test_utils.py
@@ -0,0 +1,79 @@
+from utils import *
+
+def test_one_artist():
+  data = [
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/5INjqkS1o8h1imAzPqGZBb"
+      },
+      "href": "https://api.spotify.com/v1/artists/5INjqkS1o8h1imAzPqGZBb",
+      "id": "5INjqkS1o8h1imAzPqGZBb",
+      "name": "Tame Impala",
+      "type": "artist",
+      "uri": "spotify:artist:5INjqkS1o8h1imAzPqGZBb"
+    }
+  ]
+
+  artist = getArtists(data)
+  assert len(artist) == 1
+  assert artist[0] == "Tame Impala"
+
+def test_multiple_artists():
+  data = [
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/1hLiboQ98IQWhpKeP9vRFw"
+      },
+      "href": "https://api.spotify.com/v1/artists/1hLiboQ98IQWhpKeP9vRFw",
+      "id": "1hLiboQ98IQWhpKeP9vRFw",
+      "name": "boygenius",
+      "type": "artist",
+      "uri": "spotify:artist:1hLiboQ98IQWhpKeP9vRFw"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/12zbUHbPHL5DGuJtiUfsip"
+      },
+      "href": "https://api.spotify.com/v1/artists/12zbUHbPHL5DGuJtiUfsip",
+      "id": "12zbUHbPHL5DGuJtiUfsip",
+      "name": "Julien Baker",
+      "type": "artist",
+      "uri": "spotify:artist:12zbUHbPHL5DGuJtiUfsip"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/1r1uxoy19fzMxunt3ONAkG"
+      },
+      "href": "https://api.spotify.com/v1/artists/1r1uxoy19fzMxunt3ONAkG",
+      "id": "1r1uxoy19fzMxunt3ONAkG",
+      "name": "Phoebe Bridgers",
+      "type": "artist",
+      "uri": "spotify:artist:1r1uxoy19fzMxunt3ONAkG"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/07D1Bjaof0NFlU32KXiqUP"
+      },
+      "href": "https://api.spotify.com/v1/artists/07D1Bjaof0NFlU32KXiqUP",
+      "id": "07D1Bjaof0NFlU32KXiqUP",
+      "name": "Lucy Dacus",
+      "type": "artist",
+      "uri": "spotify:artist:07D1Bjaof0NFlU32KXiqUP"
+    }
+  ]
+  artists = getArtists(data)
+  assert len(artists) == 4
+  assert "boygenius" in artists
+  assert "Julien Baker" in artists
+  assert "Phoebe Bridgers" in artists
+  assert "Lucy Dacus" in artists
+
+def test_get_genre():
+  data = "https://open.spotify.com/artist/2x9SpqnPi8rlE9pjHBwmSC"
+  genre = getGenre(data)
+  assert genre == "Art Punk"
+
+def test_get_genre_no_genre():
+  data = "https://open.spotify.com/artist/3dBOwPj9GaClkPMJXIruFP"
+  genre = getGenre(data)
+  assert genre == "N/A"
